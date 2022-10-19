d0e424e386645ed3d727b28be44f606e25e26a85
Aaditya dubey <115134350+Aaditya14x@users.noreply.github.com>
2022-10-19 22:57:49 +0530

    Add files via upload

---
+++ Calculator_in_python.py
@@ -0,0 +1,16 @@
+A = int(input("enter a number : "))
+B = int(input("enter a number : "))
+operator = input(" enter a operator '+,-,%,*,/' ")
+
+if (operator == "+"):
+ print(A+B)
+elif (operator == "-"):
+ print(A-B)
+elif (operator == "*"):
+ print(A+B)
+elif (operator == "%"):
+ print(A%B)
+elif (operator == "/"):
+ print(A/B)
+else:
+ ("Invalid Statement")
